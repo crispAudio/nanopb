d667a8db9c81cef61c806dbc0d1d00dd5b328bc8
jmueller <jmueller@crispaudio.de>
2025-02-06 15:51:11 +0100

    Add make of proto libs

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cd /tmp/generator/proto/
+make
+cd /tmp
 protoc --plugin=protoc-gen-nanopb=/tmp/generator/protoc-gen-nanopb --proto_path="/tmp/generator/proto" --proto_path="/tmp/config" /tmp/config/*.proto --nanopb_out=/tmp/config
